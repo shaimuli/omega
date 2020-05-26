c542c460cd81161fac5999a4343355d00054e64d
shaimuli <shaimuli@gmail.com>
2020-05-26 22:07:03 +0300

    asdasd
    sada

--- MobileOmega/bld/Ripple/Android/Debug/scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="http://ajax.googleapis.com/ajax/libs/jquery/2.1.2/jquery.min.js" />
-/// <reference path="index.js" />
-/// <reference path="~/app/js/app.js" />
-/// <reference path="~/app/js/albumService.js" />
-/// <reference path="~/app/js/albumServiceLocal.js" />
