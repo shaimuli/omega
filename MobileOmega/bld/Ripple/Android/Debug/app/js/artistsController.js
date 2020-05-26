a2da1232ced94f4feaa3f21ff338d7725ea09684
shaimuli <shaimuli@gmail.com>
2020-05-26 21:35:02 +0300

    add new proj

---
+++ MobileOmega/bld/Ripple/Android/Debug/app/js/artistsController.js
@@ -0,0 +1,41 @@
+﻿(function() {
+    'use strict';
+
+    var app = angular
+        .module('app')
+        .controller('artistsController', artistsController);
+
+    if(app.configuration.useLocalData)
+        artistsController.$inject = ["$scope", "$animate", "artistServiceLocal"];
+    else
+        artistsController.$inject = ["$scope", "$animate", "artistService"];
+
+
+
+    function artistsController($scope, $animate,artistService) {
+        console.log('artists controller');
+
+        var vm = this; // controller as
+        vm.artists = [];
+        vm.searchText = "";
+        vm.baseUrl = "data/";
+
+        vm.getArtists = function() {
+            return artistService.getArtists()
+                .success(function(artists) {
+                    vm.artists = artists;
+                });
+        }
+
+        $scope.$root.$on('onsearchkey', function(e, searchText) {
+            vm.searchText = searchText;
+        });
+
+        vm.getArtists();
+
+        // force explicit animation of the view and edit forms always
+        //$animate.addClass("#MainView", "slide-animation");
+
+        return;
+    }
+})();
